7302ddeb7634aed0ebf48756f0bbf4b217797bca
junghoon <jhyj3636@gmail.com>
2021-03-04 20:12:35 +0900

    UX 개선

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,37 @@
       <span class="addContainer" v-on:click="addTodo">
          <i class="fas fa-plus addBtn"></i>
       </span>
+
+      <Modal v-if="showModal" @close="showModal = false">
+         <!--
+            you can use custom content here to overwrite
+            default content
+         -->
+         <h3 slot="header">
+            경고!
+            <i class="fas fa-times closeModalBtn" @click="showModal = false"></i>
+         </h3>
+         <div slot="body">
+            아무것도 입력하지 않으셨습니다.
+         </div>
+         <h3 slot="footer">
+            Copyright
+            <button class="modal-default-button" @click="$emit('close')">
+               OK
+            </button>
+         </h3>
+      </Modal>
    </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue';
+
 export default {
    data: function() {
       return {
-         newTodoItem: ""
+         newTodoItem: "",
+         showModal: false
       }
    },
    methods: {
@@ -19,11 +42,16 @@ export default {
          if (this.newTodoItem !== '') {
             this.$emit('addTodoItem', this.newTodoItem);
             this.clearInput();
+         } else {
+            this.showModal = !this.showModal;
          }
       },
       clearInput: function() {
          this.newTodoItem = '';
       }
+   },
+   components: {
+      Modal
    }
 }
 </script>
@@ -52,5 +80,8 @@ input:focus {
 .addBtn {
   color: white;
   vertical-align: middle;
+}
+.closeModalBtn {
+   color: #42b983;
 }
 </style>
