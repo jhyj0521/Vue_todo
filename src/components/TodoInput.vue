e0584749d787fb1021226d316228f47422b467fe
junghoon <jhyj3636@gmail.com>
2021-03-05 11:43:48 +0900

    ES6 문법으로 리팩토링

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -31,14 +31,14 @@
 import Modal from './common/Modal.vue';
 
 export default {
-   data: function() {
+   data() {
       return {
          newTodoItem: "",
          showModal: false
       }
    },
    methods: {
-      addTodo: function() {
+      addTodo() {
          if (this.newTodoItem !== '') {
             this.$emit('addTodoItem', this.newTodoItem);
             this.clearInput();
@@ -46,7 +46,7 @@ export default {
             this.showModal = !this.showModal;
          }
       },
-      clearInput: function() {
+      clearInput() {
          this.newTodoItem = '';
       }
    },
