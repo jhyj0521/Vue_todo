07a14caa9055b71be30e22c334fbabff93606f5c
junghoon <jhyj3636@gmail.com>
2021-03-06 13:28:20 +0900

    Vuex설치와 리팩토링

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
      <todo-header></todo-header>
-     <todo-input v-on:addTodoItem="addOneItem"></todo-input>
-     <todo-list v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></todo-list>
-     <todo-footer v-on:clearAll="clearAllItems"></todo-footer>
+     <todo-input></todo-input>
+     <todo-list></todo-list>
+     <todo-footer></todo-footer>
   </div>
 </template>
 
@@ -14,42 +14,6 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-   data() {
-      return {
-         todoItems: []
-      }
-   },
-   methods: {
-      addOneItem(todoItem) {
-            const obj = {completed: false, item: todoItem};
-            // 여기서의 this는 현재의 컴포넌트를 가르킨다.
-            // 로컬 스토리지에 키, 값으로 저장한다.
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            this.todoItems.push(obj);
-      },
-      removeOneItem(todoItem, index) {
-         localStorage.removeItem(todoItem.item);
-         this.todoItems.splice(index, 1);
-      },
-      toggleOneItem(todoItem, index) {  
-         this.todoItems[index].completed = !this.todoItems[index].completed;  
-         localStorage.removeItem(todoItem.item);
-         localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-      },
-      clearAllItems() {
-         localStorage.clear();
-         this.todoItems = [];
-      }
-   },
-   created() {
-      if(localStorage.length > 0) {
-         for (let i = 0; i < localStorage.length; i++) {
-            if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-               this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-            }
-         }
-      }
-   },
    components: {
       TodoHeader,
       TodoInput,
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,9 @@
 <template>
    <div>
       <transition-group name="list" tag="ul">
-         <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+         <li v-for="(todoItem, index) in this.$store.state.todoItems" v-bind:key="todoItem.item" class="shadow">
             <i class="fas fa-check checkBtn" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
-            v-on:click="toggleComplete(todoItem)"></i>
+            v-on:click="toggleComplete(todoItem, index)"></i>
             <span v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</span>
             <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
                <i class="fas fa-trash-alt"></i>
@@ -15,13 +15,12 @@
 
 <script>
 export default {  
-   props: ['propsdata'],
    methods: {
       removeTodo(todoItem, index) {
-         this.$emit('removeItem', todoItem, index);
+         this.$store.commit('removeOneItem', {todoItem, index});
       },
       toggleComplete(todoItem, index) {
-         this.$emit('toggleItem', todoItem, index);
+         this.$store.commit('toggleOneItem', {todoItem, index});
       }
    }
 }
